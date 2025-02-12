7364090f662820369b0d1b9fcd01c1db2b87e958
gengorous <tenchimatou@yahoo.co.jp>
2025-02-13 01:31:09 +0900

    fix: force add start.sh

--- abc.py
+++
@@ -1,8 +0,0 @@
-from google.auth import default
-from google.cloud import storage
-
-creds, project = default()
-print(f"認証成功！プロジェクト: {project}")
-client = storage.Client(credentials=creds)
-buckets = list(client.list_buckets())
-print(f"バケット一覧: {buckets}")
